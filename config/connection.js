5b675aff20662cd2c02a4f9f6e57440efdf9e132
Stefanos Bezanis <stefbezanis@gmail.com>
2017-05-11 16:28:01 -0500

    ugh

--- config/connection.js
+++ config/connection.js
@@ -13,10 +13,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "burger",
+    port: 3306,
+    host: "localhost",
     user: "root",
     password: "",
-    database: "ismrg2pgoxnupc41"
+    database: "burgers_db"
   });
 };
 
